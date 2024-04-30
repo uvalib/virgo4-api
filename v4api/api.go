7fc43ac414c6743ae6a0f194a748a05fa3545fd8
Jason Jordan <jlj5aj@virginia.edu>
2024-04-29 21:38:05 -0400

    VIRGONEW-2730: add content advisory field for related images in image search

--- v4api/api.go
+++ v4api/api.go
@@ -163,6 +163,7 @@ type RelatedRecord struct {
 	ID              string `json:"id,omitempty"`
 	IIIFManifestURL string `json:"iiif_manifest_url,omitempty"`
 	IIIFImageURL    string `json:"iiif_image_url,omitempty"`
+	ContentAdvisory string `json:"content_advisory,omitempty"`
 }
 
 // Record is a summary of one search hit
